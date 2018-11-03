8d2b9052be693598553688c128d1d125b69773d5
Stanislaus Bosch <stanislaus.bosch@hs-bochum.de>
2018-11-03 22:20:23 +0100

    added accelerometer-sensor-path

--- api/routes/accelerometer.js
+++ api/routes/accelerometer.js
@@ -4,43 +4,45 @@ var db = require('../models/database');
 
 
 
-// GET /api/???/
+// GET /api/accelerometer
 router.get('/', function(req, res, next) {
  
   /* TODO:
-   * get all ??? from the database
+   * get all accelerometer-sensor-data from the database
    */
 
   db.pool.getConnection(function(err, con) {
     if(err) return res.status(400).send("Databse Error");
     else
-    //con.query("SELECT * FROM ???", function (err, result, fields) {
-    //  if (err) throw err;
-    //  else 
+    con.query("SELECT * FROM accelerometer", function (err, result, fields) {
+      if (err) throw err;
+      else 
             res.status(200).send("Nothing here yet");
       res.end();
       con.release();
-//    });  
+    });  
   }); 
 });
 
-// POST /api/???/
+// POST /api/accelerometer
 router.post('/', function(req, res) {
 
   /* TODO:
    * create an activity and add to the database
    */
-  //var sql = 'INSERT INTO ??? (???) VALUES ('+req.body.ID+', \''+req.body.Data+'\')';
+  var sql = 'INSERT INTO accelerometer (id, timestamp, x, y, z) VALUES (NULL, CURRENT_TIMESTAMP, \''+req.body.x+'\', \''+req.body.y+'\', \''+req.body.z+'\')';
   db.pool.getConnection(function(err, con) {
-    if(err) return res.status(400).send("Databse Error");
+    if(err) return res.status(400).send("Database Error");
     else
-//    con.query(sql, function (err, result) {
-//      if (err) throw err;
-//      else 
-        console.log("Data created and added");
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      else{
+         console.log("Data created and added");
+         res.send(req.body);
+      }
       res.end();
       con.release();
-//   });
+    });
   });  
 });
 
