5926258837f828111cf3009d55a3c31e7c4e2cb1
Stanislaus Bosch <stanislaus.bosch@hs-bochum.de>
2018-11-06 17:20:58 +0100

    fix unkown

--- api/routes/magnetometer.js
+++ api/routes/magnetometer.js
@@ -29,20 +29,20 @@ router.post('/', function(req, res) {
   /* TODO:
    * create an magnetometer-sensor-data and add to the database
    */
-  var sql = 'INSERT INTO magnetometer (id, timestamp, x, y, z) VALUES (NULL, CURRENT_TIMESTAMP, \''+req.body.x+'\', \''+req.body.y+'\', \''+req.body.z+'\')';
+  var sql = 'INSERT INTO magnetometer (id, timestamp, x, y, z, session_id) VALUES (NULL, CURRENT_TIMESTAMP, \''+req.body.x+'\', \''+req.body.y+'\', \''+req.body.z+'\', \''+req.body.session_id+'\')';
   db.pool.getConnection(function(err, con) {
-    if(err) return res.status(400).send("Database Error");
-    else
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      else{
-         console.log("Data created and added");
-         res.send(req.body);
-      }
-      res.end();
-      con.release();
+      if(err) return res.status(400).send("Databse Error");
+      else
+      con.query(sql, function (err, result) {
+        if (err) return res.status(400).send(err);
+        else {
+          console.log("Data created and added");
+          res.send(req.body);
+        }        
+        res.end();
+        con.release();
+     });
     });
-  });  
 });
 
 module.exports = router;
