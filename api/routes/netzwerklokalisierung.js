44ef0aeab1124537a4033a9818fa44b6529b5786
Stanislaus Bosch <stanislaus.bosch@hs-bochum.de>
2018-11-27 16:13:01 +0100

    added netzwerklok

--- api/routes/netzwerklokalisierung.js
+++ api/routes/netzwerklokalisierung.js
@@ -34,7 +34,55 @@ router.post('/', function (req, res) {
     temp = req.body[0];
   else
     temp = req.body;
-  var sql = 'INSERT INTO netzwerklokalisierung (id, timestamp, Latitude , Longitude, Hoehe, session_id) VALUES (NULL, CURRENT_TIMESTAMP, \'' + temp.Latitude  + '\', \'' + temp.Longitude + '\', \'' + temp.Hoehe + '\', \'' + temp.session_id + '\')';
+
+    if(!temp.hasOwnProperty('latitudeGPS')){
+      temp['latitudeGPS'] = 0;
+      temp['latitudeGPS'] = 0;
+      temp['latitudeGPS'] = 0;
+    }
+    if(!temp.hasOwnProperty('latitudeNetwork')){
+      temp['latitudeNetwork'] = 0;
+      temp['longitudeNetwork'] = 0;
+      temp['altitudeNetwork'] = 0;
+      temp['speedNetwork'] = 0;
+    }
+    if(!temp.hasOwnProperty('latitudeHighAcc')){
+      temp['latitudeHighAcc'] = 0;
+      temp['longitudeHighAcc'] = 0;
+      temp['altitudeHighAcc'] = 0;
+      temp['speedHighAcc'] = 0;
+    }
+    if(!temp.hasOwnProperty('latitudeBalanced')){
+      temp['latitudeBalanced'] = 0;
+      temp['longitudeBalanced'] = 0;
+      temp['altitudeBalanced'] = 0;
+      temp['speedBalanced'] = 0;
+    }
+    if(!temp.hasOwnProperty('latitudeLowPow')){
+      temp['latitudeLowPow'] = 0;
+      temp['longitudeLowPow'] = 0;
+      temp['altitudeLowPow'] = 0;
+      temp['speedLowPow'] = 0;
+    }
+    if(!temp.hasOwnProperty('latitudeNoPow')){
+      temp['latitudeNoPow'] = 0;
+      temp['longitudeNoPow'] = 0;
+      temp['altitudeNoPow'] = 0;
+      temp['speedNoPow'] = 0;
+    }
+
+  var sql = 'INSERT INTO netzwerklokalisierung (id, timestamp, latitudeGPS, longitudeGPS, altitudeGPS, speedGPS,' 
+    +'latitudeNetwork, longitudeNetwork, altitudeNetwork, speedNetwork,'
+    +'latitudeHighAcc, longitudeHighAcc, altitudeHighAcc, speedHighAcc,'
+    +'latitudeBalanced, longitudeBalanced, altitudeBalanced, speedBalanced,' 
+    +'latitudeLowPow, longitudeLowPow, altitudeLowPow, speedLowPow,' 
+    +'latitudeNoPow, longitudeNoPow, altitudeNoPow, speedNoPow, session_id )'
+    +' VALUES (NULL, CURRENT_TIMESTAMP, \'' + temp.latitudeGPS  + '\', \'' + temp.longitudeGPS + '\', \'' + temp.altitudeGPS + '\', \'' + temp.speedGPS 
+    + temp.latitudeNetwork  + '\', \'' + temp.longitudeNetwork + '\', \'' + temp.altitudeNetwork + '\', \'' + temp.speedNetwork
+    + temp.latitudeHighAcc  + '\', \'' + temp.longitudeHighAcc + '\', \'' + temp.altitudeHighAcc + '\', \'' + temp.speedHighAcc
+    + temp.latitudeBalanced  + '\', \'' + temp.longitudeBalanced + '\', \'' + temp.altitudeBalanced + '\', \'' + temp.speedBalanced
+    + temp.latitudeLowPow  + '\', \'' + temp.longitudeLowPow + '\', \'' + temp.altitudeLowPow + '\', \'' + temp.speedLowPow
+    + temp.latitudeNoPow  + '\', \'' + temp.longitudeNoPow + '\', \'' + temp.altitudeNoPow + '\', \'' + temp.speedNoPow + '\', \'' + temp.session_id + '\')';
   db.pool.getConnection(function (err, con) {
     if (err) return res.status(400).send("Databse Error");
     else
