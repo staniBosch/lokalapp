ff7c6844570bbf21c25541edce11c59057192d3b
Stanislaus Bosch <stanislaus.bosch@hs-bochum.de>
2018-12-18 04:41:57 +0100

    added array param for waypoint

--- api/routes/waypoint.js
+++ api/routes/waypoint.js
@@ -31,46 +31,78 @@ router.post('/', function (req, res) {
   /* TODO:
    * create an waypoint-value and add to the database
    */
-  var sql = "INSERT INTO waypoint (id, timestamp, latitude, longitude, altitude, indoor, route_name) VALUES (NULL, '"
-  + req.body.timestamp+"', '" 
-  + req.body.latitude + "', '" 
-  + req.body.longitude + "', '" 
-  + req.body.altitude + "', '" 
-  + req.body.indoor + "', '" 
-  + req.body.route_name + "')";
-  db.pool.getConnection(function (err, con) {
-    if (err) return res.status(400).send("Databse Error");
-    else
-      con.query(sql, function (err, result) {
-        if (err) res.status(400).send(err.code);
-        else {
-          console.log("Data created and added");
-          res.send(req.body);
-        }
-        res.end();
-        con.release();
+  var temp;
+  if (req.body instanceof Array) {
+    for (var i = 0; i < req.body.length; i++) {
+      var sql = "INSERT INTO waypoint (id, timestamp, latitude, longitude, altitude, indoor, route_name) VALUES (NULL, '"
+        + req.body[i].timestamp + "', '"
+        + req.body[i].latitude + "', '"
+        + req.body[i].longitude + "', '"
+        + req.body[i].altitude + "', '"
+        + req.body[i].indoor + "', '"
+        + req.body[i].route_name + "')";
+      db.pool.getConnection(function (err, con) {
+        if (err) return res.status(400).send("Databse Error");
+        else
+          con.query(sql, function (err, result) {
+            if (err) res.status(400).send(err.code);
+            else {
+              console.log("Data created and added");
+              res.send(req.body);
+            }
+            res.end();
+            con.release();
+          });
       });
-  });
-});
+    }
+  }
 
-router.get('/kml', function (req, res, next) {     
-
-  db.pool.getConnection(function (err, con) {
-      var fileName = "/../../public/tmp.kml";
-      var savedFilePath = __dirname + fileName;
+  else {
+    var sql = "INSERT INTO waypoint (id, timestamp, latitude, longitude, altitude, indoor, route_name) VALUES (NULL, '"
+      + req.body.timestamp + "', '"
+      + req.body.latitude + "', '"
+      + req.body.longitude + "', '"
+      + req.body.altitude + "', '"
+      + req.body.indoor + "', '"
+      + req.body.route_name + "')";
+    db.pool.getConnection(function (err, con) {
       if (err) return res.status(400).send("Databse Error");
       else
-          con.query("SELECT * FROM waypoint", function (err, result, fields) {
-              if (err) res.status(400).send(err.code);
-              else {
-                  var fileContents = kmlCreator.createKML(result);
-                  fs.writeFile(savedFilePath, fileContents, function (err) {
-                      if (err) console.log(err);
-                      res.status(200).download(savedFilePath, "waypoints.kml");
-                  });                    
-              }
-              con.release();
+        con.query(sql, function (err, result) {
+          if (err) res.status(400).send(err.code);
+          else {
+            console.log("Data created and added");
+            res.send(req.body);
+          }
+          res.end();
+          con.release();
+        });
+    });
+  }
+
+
+
+
+});
+
+router.get('/kml', function (req, res, next) {
+
+  db.pool.getConnection(function (err, con) {
+    var fileName = "/../../public/tmp.kml";
+    var savedFilePath = __dirname + fileName;
+    if (err) return res.status(400).send("Databse Error");
+    else
+      con.query("SELECT * FROM waypoint", function (err, result, fields) {
+        if (err) res.status(400).send(err.code);
+        else {
+          var fileContents = kmlCreator.createKML(result);
+          fs.writeFile(savedFilePath, fileContents, function (err) {
+            if (err) console.log(err);
+            res.status(200).download(savedFilePath, "waypoints.kml");
           });
+        }
+        con.release();
+      });
   });
 });
 
