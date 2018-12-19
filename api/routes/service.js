0b54d503b36f9fb8877c612ef689b92f8f47d531
Stanislaus Bosch <stanislaus.bosch@hs-bochum.de>
2018-12-19 12:56:00 +0100

    interpolieren get added

--- api/routes/service.js
+++ api/routes/service.js
@@ -63,6 +63,37 @@ router.post('/interpolieren/s', function (req, res) {
   res.end();
 });
 
+// Service fürs Interpolieren von Koordinaten in ms schritten
+router.get('/interpolieren/s/:data', function (req, res) {
+  var timefactor = 1;
+  if (data > 1)
+    if (data[0].timestamp > 90000000000)
+      timefactor = 1 / 1000;
+  var interpoliert = [{ "latitude": data[0].latitude, "longitude": data[0].longitude, "timestamp": (Math.round(data[0].timestamp * timefactor)) }];
+  //i=0
+  for (var ai = 0; ai < data.length - 1; ai++) {
+    var x0 = Number(data[ai].latitude);
+    var y0 = Number(data[ai].longitude);
+    var x1 = Number(data[ai + 1].latitude);
+    var y1 = Number(data[ai + 1].longitude);
+    var t0 = Math.round(Number(data[ai].timestamp) * timefactor);
+    var t1 = Math.round(Number(data[ai + 1].timestamp) * timefactor);
+    var tdiff = t1 - t0;
+    for (var t = 1; t < tdiff + 1; t++) {
+      var xdiff = (x1 - x0) * t / tdiff;
+      var ydiff = (y1 - y0) * t / tdiff;
+      var xtel = Number(x0) + Number(xdiff);
+      var ytel = Number(y0) + Number(ydiff);
+      var ttel = Number(t0) + Number(t);
+      interpoliert.push({ "latitude": xtel, "longitude": ytel, "timestamp": ttel });
+    }
+  }
+  res.status(200).send(interpoliert);
+  res.end();
+});
+
+
+
 
 router.get('/kml/:jsonarray', function (req, res){
   var data = JSON.parse(req.params.jsonarray);
