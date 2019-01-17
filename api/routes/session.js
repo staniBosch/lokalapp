745d65238a4a61b03fc2bd820185acf9a1395680
Stanislaus Bosch <stanislaus.bosch@hs-bochum.de>
2019-01-17 02:01:25 +0100

    session order by timestamp if exist

--- api/routes/session.js
+++ api/routes/session.js
@@ -5,65 +5,69 @@ var db = require('../models/database');
 
 
 // GET /api/session
-router.get('/', function(req, res, next) {
- 
+router.get('/', function (req, res, next) {
+
   /* TODO:
    * get all session-data from the database
    */
 
-  db.pool.getConnection(function(err, con) {
-    if(err) return res.status(400).send("Databse Error");
+  db.pool.getConnection(function (err, con) {
+    if (err) return res.status(400).send("Databse Error");
     else
-    con.query("SELECT * FROM session", function (err, result, fields) {
-      if (err) res.status(400).send(err.code);
-      else res.status(200).send(result);
-      res.end();
-      con.release();
-    });  
-  }); 
+      con.query("SELECT * FROM session", function (err, result, fields) {
+        if (err) res.status(400).send(err.code);
+        else res.status(200).send(result);
+        res.end();
+        con.release();
+      });
+  });
 });
 
 // GET /api/session/:id/:sensor
-router.get('/id/:id/:sensor', function(req, res, next) {
- 
+router.get('/id/:id/:sensor', function (req, res, next) {
+
   /* TODO:
    * get all session-data from the database
    */
   var sensor = req.params.sensor;
   var id = req.params.id;
-  var sql = "SELECT * FROM session inner join "+ sensor+" on session.id="+sensor+".session_id where session.id="+id;
-  db.pool.getConnection(function(err, con) {
-    if(err) return res.status(400).send("Databse Error");
+  var sql = "";
+  if (sensor == "lokalisierung")
+    sql = "SELECT * FROM session inner join " + sensor + " on session.id=" + sensor + ".session_id where session.id=" + id + " ODER BY timestamp";
+  else
+    sql = "SELECT * FROM session inner join " + sensor + " on session.id=" + sensor + ".session_id where session.id=" + id;
+  db.pool.getConnection(function (err, con) {
+    if (err) return res.status(400).send("Databse Error");
     else
-    con.query(sql , function (err, result, fields) {
-      if (err) res.status(400).send(err.code);
-      else res.status(200).send(result);
-      res.end();
-      con.release();
-    });  
-  }); 
+      con.query(sql, function (err, result, fields) {
+        if (err) res.status(400).send(err.code);
+        else res.status(200).send(result);
+        res.end();
+        con.release();
+      });
+  });
 });
 
 // POST /api/session/
-router.post('/', function(req, res) {
+router.post('/', function (req, res) {
 
   /* TODO:
    * create an session-value and add to the database
    */
-  var sql = 'INSERT INTO session (id, timestamp, value) VALUES (NULL, CURRENT_TIMESTAMP, \''+req.body.value+'\')';
-  db.pool.getConnection(function(err, con) {
-    if(err) return res.status(400).send("Databse Error");
+  var sql = 'INSERT INTO session (id, timestamp, value) VALUES (NULL, CURRENT_TIMESTAMP, \'' + req.body.value + '\')';
+  db.pool.getConnection(function (err, con) {
+    if (err) return res.status(400).send("Databse Error");
     else
-    con.query(sql, function (err, result) {
-      if (err) res.status(400).send(err.code);
-      else {
-        console.log("Data created and added");        
-        res.send({"id" :result.insertId});
-      }
-      res.end();
-      con.release();
-   });
-  });  
+      con.query(sql, function (err, result) {
+        if (err) res.status(400).send(err.code);
+        else {
+          console.log("Data created and added");
+          res.send({ "id": result.insertId });
+        }
+        res.end();
+        con.release();
+      });
+  });
 });
 
 module.exports = router;
