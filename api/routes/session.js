a9ef59c68ce45008c20d40e1c1c5ac78a93d7579
Stanislaus Bosch <stanislaus.bosch@hs-bochum.de>
2019-01-17 03:55:12 +0100

    small fix

--- api/routes/session.js
+++ api/routes/session.js
@@ -33,7 +33,7 @@ router.get('/id/:id/:sensor', function (req, res, next) {
   var id = req.params.id;
   var sql = "";
   if (sensor == "lokalisierung" || sensor == "messwerte")
-    sql = "SELECT * FROM session inner join " + sensor + " on session.id=" + sensor + ".session_id where session.id=" + id + " ORDER BY lokalisierung.timestamp";
+    sql = "SELECT * FROM session inner join " + sensor + " on session.id=" + sensor + ".session_id where session.id=" + id + " ORDER BY "+sensor+".timestamp";
   else
     sql = "SELECT * FROM session inner join " + sensor + " on session.id=" + sensor + ".session_id where session.id=" + id;
   db.pool.getConnection(function (err, con) {
