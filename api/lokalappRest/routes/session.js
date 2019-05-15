227a2cd2d161b10675d6ecc6106dec8257c53d17
staniBosch <stani300191@yahoo.de>
2019-05-16 00:13:34 +0200

    added timestamp filter req in session

--- api/lokalappRest/routes/session.js
+++ api/lokalappRest/routes/session.js
@@ -48,6 +48,32 @@ router.get('/id/:id/:sensor', function (req, res, next) {
   });
 });
 
+// GET /api/session/:id/:sensor/:timestamp
+router.get('/id/:id/:sensor/:timestamp', function (req, res, next) {
+
+  /* TODO:
+   * get all session-data from the database after the time of the given timestamp
+   */
+  var timestamp = req.params.timestamp;
+  var sensor = req.params.sensor;
+  var id = req.params.id;
+  var sql = "";
+  if (sensor == "lokalisierung" || sensor == "messwerte")
+    sql = "SELECT * FROM session inner join " + sensor + " on session.id=" + sensor + ".session_id where session.id=" + id + " ORDER BY "+sensor+".timestamp";
+  else
+    sql = "SELECT * FROM session inner join " + sensor + " on session.id=" + sensor + ".session_id where session.id=" + id + " AND "+ sensor + ".timestamp <"+ timestamp;
+  db.pool.getConnection(function (err, con) {
+    if (err) return res.status(400).send("Databse Error");
+    else
+      con.query(sql, function (err, result, fields) {
+        if (err) res.status(400).send(err.code);
+        else res.status(200).send(result);
+        res.end();
+        con.release();
+      });
+  });
+});
+
 // POST /api/session/
 router.post('/', function (req, res) {
 
